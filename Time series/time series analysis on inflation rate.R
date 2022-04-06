1f562aac14e0ae5c4ed533134443010b153c47c1
Mojisibe <95708219+Mojisibe@users.noreply.github.com>
2022-04-06 17:51:05 +0100

    Update time series analysis on inflation rate.R

--- Time series/time series analysis on inflation rate.R
+++ Time series/time series analysis on inflation rate.R
@@ -4,20 +4,22 @@ library(fpp2)
 library(forecast)
 
 #read data
-inflation_ts <- read.csv("Inflation.csv", header = T)
-View(inflation_ts)
+inflation_ts <- read.csv("Inflation.csv", header = T) #inflation_ts is the name assigned to the dataset
+View(inflation_ts) # View the dataset
 
-#convert data to time series format
-inflation_ts <- ts(inflation_ts[,2], start = c(2010, 1), frequency = 12)
+#convert data to time series format in other to for r to see it as a time series data
+inflation_ts <- ts(inflation_ts[,2], start = c(2010, 1), frequency = 12) # inflation_ts[,2] is used to choose the second column since it is the column that contain the dataset
+# start = c(2001,1) depicts that the data that is used for the analysis is starting from january, 2001
+# frequency = 12 depict that data has a frequency of 12 month
 
 #plot the time series dataset to observe it component
-plot(inflation_ts, ylab = "inflation rate")
-decompose_data <- decompose(inflation_ts, type = "additive")
+plot(inflation_ts, ylab = "inflation rate") # This shows the component of time series in the data
+decompose_data <- decompose(inflation_ts, type = "additive") # This decompose the dataset to remove seasonality and irregularities in the data
 decompose_data <- decompose(inflation_ts, type = "multiplicative")
-plot(decompose_data)
+plot(decompose_data) # This plot the data to shows the trend in the data
 
 #test of stationarity
-adf.test(inflation_ts)
+adf.test(inflation_ts) # This is will observe the stationarity in the data
 
 #fit the model
 fit <- auto.arima(inflation_ts)
